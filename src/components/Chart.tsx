2b636069e1543f388f3a643e6f4c07886e3016bc
chenyuhao <cccchenyuhao@gmail.com>
2023-05-02 03:49:53 +0800

    rect and x-axis

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,8 +1,8 @@
 import ChartContainer from "./ChartContainer"
 import { useState } from "react"
 import { css } from "@emotion/react"
-
-function getRandomArray(n = 10) {
+import * as d3 from "d3"
+function getRandomArray(n = 20) {
   return Array.from(new Array(n), () => {
     return Math.round(Math.random() * 100)
   })
@@ -21,6 +21,22 @@ function Chart() {
 
   const innerWidth = viewWidth - margin.left - margin.right
   const innerHeight = viewHeight - margin.top - margin.bottom
+
+  // const a = Array.from(d3.range(0, data.length - 1), n => n + '')
+
+  const xScale = d3
+    .scaleBand()
+    .domain(Array.from(d3.range(0, data.length), (n) => String(n)))
+    .range([0, innerWidth])
+    .padding(0.2)
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data) ?? 0])
+    .range([innerHeight, 0])
+
+  // const a = xScale.step()
+
   return (
     <div>
       <ChartContainer
@@ -28,7 +44,64 @@ function Chart() {
         viewHeight={viewHeight}
         margin={margin}
       >
-        <rect x={0} y={0} width={innerWidth} height={innerHeight} fill="red" />
+        {/* x 轴 */}
+        <g transform={`translate(0, ${innerHeight})`}>
+          <line x1={0} y1={0} x2={innerWidth} y2={0} stroke="black" />
+          {data.map((n, i) => {
+            return (
+              <g key={i}>
+                <text
+                  x={(xScale(String(i)) as number) + xScale.bandwidth() / 2}
+                  y={2}
+                  textAnchor="middle"
+                  alignmentBaseline="hanging"
+                  style={{
+                    fontSize: 8,
+                  }}
+                >
+                  {i}
+                </text>
+                <line></line>
+              </g>
+            )
+          })}
+        </g>
+
+        {/* y 轴 */}
+        <g>
+          <line
+            x1={0}
+            y1={0}
+            x2={0}
+            y2={innerHeight}
+            stroke="black"
+            strokeWidth={1}
+          />
+          {data.map((n, i) => {
+            // <g>
+            //   <text></text>
+            //   <line></line>
+            // </g>
+            return null
+          })}
+        </g>
+
+        {/* {柱状图} */}
+        {data.map((n, i) => {
+          return (
+            <rect
+              key={i}
+              x={xScale(String(i))}
+              y={yScale(n)}
+              width={xScale.bandwidth()}
+              height={innerHeight - yScale(n)}
+            />
+          )
+        })}
+        {/* xaxis
+        yaxis
+        shape
+        label */}
       </ChartContainer>
     </div>
   )
