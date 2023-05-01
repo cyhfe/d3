f50d84bd30306ccde0aeb2ba1e9b70a44f5e417e
chenyuhao <cccchenyuhao@gmail.com>
2023-05-02 06:13:22 +0800

    axis

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,6 +2,7 @@ import ChartContainer from "./ChartContainer"
 import { useState } from "react"
 import { css } from "@emotion/react"
 import * as d3 from "d3"
+
 function getRandomArray(n = 20) {
   return Array.from(new Array(n), () => {
     return Math.round(Math.random() * 100)
@@ -10,32 +11,37 @@ function getRandomArray(n = 20) {
 function Chart() {
   const [data, setData] = useState(() => getRandomArray())
 
-  const viewWidth = 300
-  const viewHeight = 150
+  const viewWidth = 500
+  const viewHeight = 250
   const margin = {
-    top: 20,
-    bottom: 20,
-    left: 20,
-    right: 20,
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50,
   }
 
   const innerWidth = viewWidth - margin.left - margin.right
   const innerHeight = viewHeight - margin.top - margin.bottom
 
-  // const a = Array.from(d3.range(0, data.length - 1), n => n + '')
+  const xDomain = Array.from(d3.range(0, data.length), (n) => String(n))
 
   const xScale = d3
     .scaleBand()
-    .domain(Array.from(d3.range(0, data.length), (n) => String(n)))
+    .domain(xDomain)
     .range([0, innerWidth])
     .padding(0.2)
 
   const yScale = d3
     .scaleLinear()
-    .domain([0, d3.max(data) ?? 0])
+    .domain([0, 100])
     .range([innerHeight, 0])
+    .nice()
 
-  // const a = xScale.step()
+  const yTicks = yScale.ticks(10)
+
+  const count = Math.min(data.length, 10)
+  const ticks = d3.ticks(0, data.length - 1, count)
+  console.log(yTicks)
 
   return (
     <div>
@@ -45,30 +51,41 @@ function Chart() {
         margin={margin}
       >
         {/* x 轴 */}
-        <g transform={`translate(0, ${innerHeight})`}>
+        <g transform={`translate(0, ${innerHeight})`} className="x-axis">
           <line x1={0} y1={0} x2={innerWidth} y2={0} stroke="black" />
-          {data.map((n, i) => {
+          {ticks.map((t) => {
+            const offset =
+              (xScale(String(t)) as number) + xScale.bandwidth() / 2
             return (
-              <g key={i}>
+              <g key={t} transform={`translate(${offset}, 0)`}>
                 <text
-                  x={(xScale(String(i)) as number) + xScale.bandwidth() / 2}
-                  y={2}
+                  x={0}
+                  y={6}
                   textAnchor="middle"
                   alignmentBaseline="hanging"
-                  style={{
-                    fontSize: 8,
-                  }}
+                  fontSize={8}
                 >
-                  {i}
+                  {t}
                 </text>
-                <line></line>
+                <line x1={0} y1={0} x2={0} y2={3} stroke="black"></line>
               </g>
             )
           })}
+
+          {/* label */}
+          <text
+            x={innerWidth / 2}
+            y={20}
+            textAnchor="middle"
+            alignmentBaseline="hanging"
+            fontSize={10}
+          >
+            Index
+          </text>
         </g>
 
         {/* y 轴 */}
-        <g>
+        <g className="y-axis">
           <line
             x1={0}
             y1={0}
@@ -77,27 +94,39 @@ function Chart() {
             stroke="black"
             strokeWidth={1}
           />
-          {data.map((n, i) => {
-            // <g>
-            //   <text></text>
-            //   <line></line>
-            // </g>
-            return null
+          {yTicks.map((v) => {
+            return (
+              <g transform={`translate(0, ${yScale(v)})`} key={v}>
+                <text
+                  fontSize={8}
+                  textAnchor="end"
+                  alignmentBaseline="central"
+                  x={-6}
+                  y={0}
+                >
+                  {v}
+                </text>
+                <line x1={0} y1={0} x2={-3} y2={0} stroke="black" />
+              </g>
+            )
           })}
         </g>
 
         {/* {柱状图} */}
-        {data.map((n, i) => {
-          return (
-            <rect
-              key={i}
-              x={xScale(String(i))}
-              y={yScale(n)}
-              width={xScale.bandwidth()}
-              height={innerHeight - yScale(n)}
-            />
-          )
-        })}
+        <g className="shape">
+          {data.map((n, i) => {
+            return (
+              <rect
+                key={i}
+                x={xScale(String(i))}
+                y={yScale(n)}
+                width={xScale.bandwidth()}
+                height={innerHeight - yScale(n)}
+              />
+            )
+          })}
+        </g>
+
         {/* xaxis
         yaxis
         shape
