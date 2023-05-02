2608d917f639ad3a653aeb300f4d2d550ddd9c6b
chenyuhao <cccchenyuhao@gmail.com>
2023-05-02 20:23:59 +0800

    animation ordered

--- src/components/BarShape.tsx
+++ src/components/BarShape.tsx
@@ -13,96 +13,59 @@ function BarShape({ data, innerHeight, xScale, yScale }: BarShapeProps) {
     function draw() {
       console.log("draw")
 
+      const exitTransition = d3.transition().duration(600)
+
+      const updateTransition = exitTransition.transition().duration(600)
+
+      const enterTransition = updateTransition.transition().duration(600)
+
       const rects = d3.select(barGroup.current).selectAll("rect")
 
       rects
         .data(data, (d) => d as number)
         .join(
-          // "rect"
-          (enter) => {
-            const nextEnter = enter
+          (enter) =>
+            enter
               .append("rect")
               .attr("x", (_, i) => xScale(String(i)) as number)
               .attr("y", innerHeight)
               .attr("width", xScale.bandwidth())
               .attr("height", 0)
               .call((enter) =>
-                enter.call((enter) =>
-                  enter
-                    .transition()
-                    .duration(1000)
-                    .attr("y", (d) => yScale(d))
-                    .attr("height", (d) => innerHeight - yScale(d))
-                )
-              )
-            console.log(nextEnter)
-            return nextEnter
-          },
+                enter
+                  .transition(enterTransition)
+                  .attr("y", (d) => yScale(d))
+                  .attr("height", (d) => innerHeight - yScale(d))
+              ),
+
           (update) =>
             update
               .attr("y", (d) => yScale(d))
-              // .attr("width", xScale.bandwidth())
               .attr("height", (d) => innerHeight - yScale(d))
               .call((update) =>
                 update
-                  .transition()
-                  .duration(1000)
+                  .transition(updateTransition)
                   .attr("x", (_, i) => xScale(String(i)) as number)
               ),
           (exit) => {
-            console.log(exit, "exit")
-            return exit.attr("fill", "red").remove()
-          }
-          // (enter) =>
-          //   enter
-          //     .append("rect")
-          //     .attr("x", (_, i) => xScale(String(i)) as number)
-          //     .attr("y", innerHeight)
-          //     .attr("height", 0)
-          //     .attr("width", xScale.bandwidth())
-          //     .attr("fill", "blue")
-          //     .call((enter) =>
-          //       enter
-          //         .transition()
-          //         .duration(1000)
-          //         .attr("height", (v) => innerHeight - yScale(v))
-          //         .attr("y", (v) => yScale(v))
-          //         .attr("fill", "black")
-          //     )
-          // (update) => {
-          //   console.log(update)
+            console.log(exit)
 
-          //   return update
-          //     .attr("fill", "green")
-          //     .call((update) =>
-          //       update
-          //         .transition()
-          //         .delay(1000)
-          //         .duration(1000)
-          //         .attr("fill", "black")
-          //     )
-          // },
-          // (exit) => {
-          //   return exit
-          //     .attr("fill", "red")
-          //     .call((exit) =>
-          //       exit
-          //         .transition()
-          //         .delay(1000)
-          //         .duration(1000)
-          //         .attr("fill", "black")
-          //         .remove()
-          //     )
-          // }
+            return (
+              exit
+                // .attr("y", (d) => yScale(d))
+                // .attr("height", (d) => innerHeight - yScale(d))
+                .call((exit) =>
+                  exit
+                    .transition(exitTransition)
+                    .attr("y", innerHeight)
+                    .attr("height", 0)
+                    .attr("fill", "red")
+                    .remove()
+                )
+            )
+          }
         )
-      // .attr("x", (_, i) => xScale(String(i)) as number)
-      // .attr("y", (d) => yScale(d))
-      // .attr("width", xScale.bandwidth())
-      // .attr("height", (d) => innerHeight - yScale(d))
-      // x={xScale(String(i))}
-      // y={yScale(n)}
-      // width={xScale.bandwidth()}
-      // height={innerHeight - yScale(n)}
+
       return rects
     },
     [data, innerHeight, xScale, yScale]
