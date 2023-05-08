6c0a0b7b1fbc116610a4e5579c78ffc24f78908f
chenyuhao <cccchenyuhao@gmail.com>
2023-05-09 06:51:54 +0800

    root redirect

--- src/Router.tsx
+++ src/Router.tsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Navigate to="/sort-algorithm" />,
+        element: <Navigate to="/midi-viz" />,
       },
       {
         path: "/midi-viz",
